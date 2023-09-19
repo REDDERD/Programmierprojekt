85a60aedb6139d781501b3fd15dd1080a91fb536
cepurwin <cedric.purwin@outlook.de>
2023-09-19 09:45:09 +0200

    Tests mostly fixed

--- src/app/home/chart-container/chart-container.component.spec.ts
+++ src/app/home/chart-container/chart-container.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChartContainerComponent } from './chart-container.component';
+import {ChartComponent} from "./chart/chart.component";
 
-describe('ChartComponent', () => {
+describe('ChartContainerComponent', () => {
   let component: ChartContainerComponent;
   let fixture: ComponentFixture<ChartContainerComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ChartContainerComponent]
+      declarations: [
+        ChartContainerComponent,
+        ChartComponent
+      ]
     });
     fixture = TestBed.createComponent(ChartContainerComponent);
     component = fixture.componentInstance;
--- src/app/home/input/input.component.spec.ts
+++ src/app/home/input/input.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InputComponent } from './input.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {ChartComponent} from "../chart-container/chart/chart.component";
 
 describe('InputComponent', () => {
   let component: InputComponent;
@@ -8,7 +10,13 @@ describe('InputComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [InputComponent]
+      declarations: [
+        InputComponent,
+        ChartComponent
+      ],
+      imports: [
+        MatFormFieldModule
+      ]
     });
     fixture = TestBed.createComponent(InputComponent);
     component = fixture.componentInstance;
