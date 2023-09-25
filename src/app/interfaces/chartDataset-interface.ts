993a08913fdad4d82c7700f806ff6785849aec52
REDDERD <143699981+REDDERD@users.noreply.github.com>
2023-09-25 21:46:23 +0200

    Sync test-deployment with main (#31)
    * Chart working with Mock-Data
    
    * Removed unused import
    
    * Installed Playwright for E2E-Testing
    
    * Installed ESLint
    
    * Added Linting as GitHub Action
    
    * Removed Linting as GitHub Action
    
    * csv inport (#24)
    
    * front without logic implemented
    
    * läuft mit einem Fehler
    
    * Deutsch
    
    * Chart working with Mock-Data
    
    * Removed unused import
    
    * Update sync-test-deploy-branch.yaml (#25)
    
    * Update sync-test-deploy-branch.yaml
    
    * Update sync-test-deploy-branch.yaml
    
    * Finalized sync-test-deploy-branch.yaml (#28)
    
    Should work now. Was tested in separate repo
    
    ---------
    
    Co-authored-by: cepurwin <cedric.purwin@outlook.de>
    Co-authored-by: david-2031 <113417732+david-2031@users.noreply.github.com>

---
+++ src/app/interfaces/chartDataset-interface.ts
@@ -0,0 +1,11 @@
+export interface ChartDatasetInterface {
+  "label": string,
+  "data": Object
+}
+
+export interface CentroidDatesetInterface {
+  "label": string,
+  "data": Object,
+  "pointStyle": string,
+  "radius": number
+}
