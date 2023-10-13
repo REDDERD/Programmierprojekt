7f591a0e63d85a5a8c11a8272ac4673a5a924773
cepurwin <cedric.purwin@outlook.de>
2023-10-13 14:57:08 +0200

    Fixed ngAfterViewInit Bug and all Tests

--- src/app/home/chart-container/table/table.component.spec.ts
+++ src/app/home/chart-container/table/table.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { TableComponent } from './table.component'
+import { MatTableModule } from '@angular/material/table'
 
 describe('TableComponent', () => {
   let component: TableComponent
@@ -8,7 +9,12 @@ describe('TableComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TableComponent]
+      declarations: [
+        TableComponent
+      ],
+      imports: [
+        MatTableModule
+      ]
     })
     fixture = TestBed.createComponent(TableComponent)
     component = fixture.componentInstance
--- src/app/home/input/input.component.spec.ts
+++ src/app/home/input/input.component.spec.ts
@@ -3,6 +3,16 @@ import { type ComponentFixture, TestBed } from '@angular/core/testing'
 import { InputComponent } from './input.component'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { ChartComponent } from '../chart-container/chart/chart.component'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { HttpClientModule } from '@angular/common/http'
+import { MatIconModule } from '@angular/material/icon'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatSelectModule } from '@angular/material/select'
+import { MatButtonToggleModule } from '@angular/material/button-toggle'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ReactiveFormsModule } from '@angular/forms'
+import { MatInputModule } from '@angular/material/input'
 
 describe('InputComponent', () => {
   let component: InputComponent
@@ -15,7 +25,17 @@ describe('InputComponent', () => {
         ChartComponent
       ],
       imports: [
-        MatFormFieldModule
+        MatFormFieldModule,
+        MatSnackBarModule,
+        HttpClientModule,
+        MatIconModule,
+        MatExpansionModule,
+        MatSelectModule,
+        MatButtonToggleModule,
+        MatSlideToggleModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        MatInputModule
       ]
     })
     fixture = TestBed.createComponent(InputComponent)
