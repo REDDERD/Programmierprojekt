3e3a0fd0962ff290fb21852b653495b8e42e0416
khuepp <143700333+khuepp@users.noreply.github.com>
2023-10-15 15:26:06 +0200

    Fixed a few Bugs in KMeans Service and added Tests for KMeans Service

--- src/app/home/home-services/kmeans-local.service.spec.ts
+++ src/app/home/home-services/kmeans-local.service.spec.ts
@@ -13,4 +13,103 @@ describe('KmeansLocalService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy()
   })
+
+  it('should return the specified number of clusters for 2D numerical data', (done) => {
+    const file = convertToBlobFile(numerical2D)
+    const k = 2
+    const indices = [0, 1]
+    service.performKMeans(file, k, false, 'EUCLIDEAN', indices).then(result => {
+      expect(result.cluster.length).toEqual(k)
+      done()
+    })
+      .catch(error => {
+        fail(`Test failed due to error: ${error}`)
+        done()
+      })
+  })
+
+  it('should return 2 clusters using Elbow-Method using 2D numerical data', (done) => {
+    const file = convertToBlobFile(numerical2D)
+    const k = 0
+    const indices = [0, 1]
+    service.performKMeans(file, k, true, 'EUCLIDEAN', indices).then(result => {
+      expect(result.cluster.length).toEqual(2)
+      done()
+    })
+      .catch(error => {
+        fail(`Test failed due to error: ${error}`)
+        done()
+      })
+  })
+
+  it('should return the specified number of clusters using 2D categorical data and manhattan distance', (done) => {
+    const file = convertToBlobFile(categorical2D)
+    const k = 3
+    const indices = [0, 1]
+    service.performKMeans(file, k, false, 'MANHATTAN', indices).then(result => {
+      expect(result.cluster.length).toEqual(3)
+      done()
+    })
+      .catch(error => {
+        fail(`Test failed due to error: ${error}`)
+        done()
+      })
+  })
+
+  it('should return the specified number of clusters using 3D categorical data and euclidian distance', (done) => {
+    const file = convertToBlobFile(numerical3D)
+    const k = 3
+    const indices = [0, 1, 2]
+    service.performKMeans(file, k, false, 'EUCLIDEAN', indices).then(result => {
+      expect(result.cluster.length).toEqual(3)
+      done()
+    })
+      .catch(error => {
+        fail(`Test failed due to error: ${error}`)
+        done()
+      })
+  })
 })
+
+function convertToCSV (data: Array<Array<string | number>>): string {
+  return data.map(row => row.join(',')).join('\n')
+}
+
+function convertToBlobFile (data: Array<Array<string | number>>): File {
+  const csvString = convertToCSV(data)
+  const blob = new Blob([csvString], { type: 'text/csv' })
+  return new File([blob], 'data.csv', { type: 'text/csv' })
+}
+
+const numerical2D: Array<Array<string | number>> = [
+  ['X', 'Y'],
+  [1, 2],
+  [1.5, 1.8],
+  [2, 2.2],
+  [8, 9],
+  [8.5, 9.2],
+  [7.8, 8.7]
+]
+
+const categorical2D: Array<Array<string | number>> = [
+  ['X', 'Y'],
+  ['red', 'circle'],
+  ['red', 'square'],
+  ['blue', 'circle'],
+  ['blue', 'square'],
+  ['green', 'circle'],
+  ['green', 'square']
+]
+
+const numerical3D: Array<Array<string | number>> = [
+  ['X', 'Y', 'Z'],
+  [1, 2, 3],
+  [1.2, 2.1, 3.1],
+  [0.9, 1.9, 2.9],
+  [8, 9, 10],
+  [8.1, 9.1, 10.1],
+  [7.9, 8.9, 9.9],
+  [4, 5, 6],
+  [4.1, 5.1, 6.1],
+  [3.9, 4.9, 5.9]
+]
