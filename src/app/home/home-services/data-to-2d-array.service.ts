54b1ef3f2dc74bb454209a6921806c417ec5d6e5
khuepp <143700333+khuepp@users.noreply.github.com>
2023-10-17 23:01:55 +0200

    Added Comments for Data and KMeans Service

--- src/app/home/home-services/data-to-2d-array.service.ts
+++ src/app/home/home-services/data-to-2d-array.service.ts
@@ -6,8 +6,13 @@ import * as xlsx from 'node-xlsx'
 })
 
 export class DataTo2dArrayService {
+  /**
+   * Converts the provided file into a 2D array of strings.
+   */
   async dataTo2DArray (file: File): Promise<string[][]> {
+    // Determine the file extension
     const fileExtension = file.name.split('.').pop()?.toLowerCase()
+    // Process the file based on its extension
     if (fileExtension === 'csv') {
       return await this.csvTo2DArray(file)
     } else if (fileExtension === 'xlsx') {
@@ -17,48 +22,65 @@ export class DataTo2dArrayService {
     }
   }
 
+  /**
+   * Converts the provided Excel file into a 2D array of strings.
+   */
   private async excelTo2DArray (file: File): Promise<string[][]> {
     return await new Promise<string[][]>((resolve, reject) => {
       const fileReader = new FileReader()
-
+      // Event handler for successful file read
       fileReader.onload = (event) => {
         try {
+          // Convert the file content to an ArrayBuffer
           const buffer = event.target?.result as ArrayBuffer
-          const data = xlsx.parse(buffer) // Dies gibt ein Array von Arbeitsblättern zurück
-
+          // Parse the buffer using node-xlsx library to get an array of worksheets
+          const data = xlsx.parse(buffer)
+          // Check if the parsed data is valid and not empty
           if (data.length > 0 && Array.isArray(data[0]?.data) && data[0].data.length > 0 && !(data[0].data.length === 1 && data[0].data[0][0] === '')) {
             resolve(data[0].data as string[][])
           } else {
             reject(new Error('File is empty'))
           }
         } catch (error) {
+          // Handle any parsing errors
           if (error instanceof Error) {
             reject(new Error(error.message))
           }
         }
       }
+      // Event handler for file read errors
       fileReader.onerror = () => {
-        reject(new Error('Failed to read the file.'))
+        reject(new Error('Failed to process the Excel file.'))
       }
-
+      // Start reading the file as an ArrayBuffer
       fileReader.readAsArrayBuffer(file)
     })
   }
 
+  /**
+   * Converts the content of a CSV file into a 2D array.
+   */
   private async csvTo2DArray (file: File): Promise<string[][]> {
     return await new Promise<string[][]>((resolve, reject) => {
       const fileReader = new FileReader()
+      // Event handler for successful file read
       fileReader.onload = () => {
         try {
+          // Convert the file content to a string
           const content = fileReader.result as string
+          // Detect the delimiter used in the CSV
           const delimiter = this.detectDelimiter(content)
+          // Split the content into rows and filter out any empty rows
           let rows = content.split(/\r\n|\n|\r/)
           rows = rows.filter(row => row.trim().length > 0)
+          // If there are no valid rows, resolve with an empty array
           if (rows.length === 0) {
             resolve([])
             return
           }
+          // Split each row into its columns based on the detected delimiter
           const data = rows.map(row => row.split(delimiter))
+          // Resolve the promise with the processed data
           resolve(data)
         } catch (error) {
           if (error instanceof Error) {
@@ -66,13 +88,21 @@ export class DataTo2dArrayService {
           }
         }
       }
+      // Event handler for file read errors
       fileReader.onerror = () => {
         reject(new Error('Failed to process the CSV file.'))
       }
+      // Start reading the file as text
       fileReader.readAsText(file)
     })
   }
 
+  /**
+   * Detects the delimiter used in a CSV string.
+   * The function checks common delimiters (comma, semicolon, tab, and pipe)
+   * and determines the best fit based on the consistency of its occurrence
+   * in the first few lines of the CSV data.
+   */
   private detectDelimiter (csvData: string): string {
     // List of potential delimiters
     const potentialDelimiters = [',', ';', '\t', '|']
@@ -82,6 +112,7 @@ export class DataTo2dArrayService {
     let bestDelimiter = ''
     let bestDelimiterCount = 0
 
+    // Iterate over each potential delimiter to determine the best fit
     for (const delimiter of potentialDelimiters) {
       let currentDelimiterCount = 0
       const counts = []
--- src/app/home/home-services/kmeans-local.service.ts
+++ src/app/home/home-services/kmeans-local.service.ts
@@ -13,56 +13,76 @@ export class KmeansLocalService {
   data: string[][] = []
   clusters: any[] = []
 
+  /**
+   * Calculates the Euclidean distance between two points.
+   */
   private euclideanDistance (pointA: number[], pointB: number[]): number {
     return Math.sqrt(pointA.reduce((sum, value, index) => sum + Math.pow(value - pointB[index], 2), 0))
   }
 
+  /**
+   * Calculates the Manhattan distance between two points.
+   */
   private manhattanDistance (pointA: number[], pointB: number[]): number {
     return pointA.reduce((sum, value, index) => sum + Math.abs(value - pointB[index]), 0)
   }
 
+  /**
+   * Determines the optimal number of clusters for KMeans clustering using the Elbow Method.
+   * The Elbow Method calculates the Sum of Squared Distances (SSD) for different numbers of clusters
+   * and identifies the "elbow" point where the rate of decrease sharply changes.
+   */
   private elbowMethod (data: number[][], maxClusters: number, distanceMetric: string): number {
-    const ssd: number[] = [] // Sum of Squared Distances for different cluster numbers
+    const ssd: number[] = [] // Sum of Squared Distances for different cluster numbers.
     const distanceFunction = distanceMetric === 'EUCLIDEAN' ? this.euclideanDistance : this.manhattanDistance
 
+    // Iterate through potential cluster numbers up to maxClusters or the data length.
     for (let i = 1; i <= maxClusters && i < data.length; i++) {
       const result = KMeans(data, i, { distanceFunction })
       let currentSSD = 0
+      // Calculate the Sum of Squared Distances (SSD) for each data point to its centroid.
       for (let j = 0; j < data.length; j++) {
         const centroid = result.centroids[result.clusters[j]]
         currentSSD += distanceFunction(data[j], centroid) ** 2
       }
       ssd.push(currentSSD)
     }
-    // Calculate the rate of change of SSD (first derivative)
+    // Calculate the rate of change of SSD (first derivative).
     const ratesOfChange = ssd.slice(1).map((value, index) => ssd[index] - value)
-    // Calculate the second derivative
+    // Calculate the second derivative.
     const secondDerivative = ratesOfChange.slice(1).map((value, index) => ratesOfChange[index] - value)
-    // Find the index of the maximum value in the second derivative
+    // Find the index of the maximum value in the second derivative.
     const elbowPoint = secondDerivative.indexOf(Math.max(...secondDerivative))
-    // Convert SSD values to an array of x and y coordinates
-    // const coordinates = ssd.map((value, index) => ({ x: index + 1, y: value }))
-
-    // Return the optimal number of clusters
+    // Return the optimal number of clusters.
     return elbowPoint + 2 // +2 because the index is 0-based and we started from k=1
   }
 
+  /**
+   * Removes rows containing NaN values from the given 2D numerical data.
+   * If more than 25% of the rows are removed due to NaN values, an error is thrown.
+   */
   private removeNaN (dataAsNumbers: number[][]): number[][] {
-    // Filter out all invalid arrays
+    // Filter out rows containing NaN values.
     const filteredData = dataAsNumbers.filter(row => {
       return row.every(value => !isNaN(value))
     })
-    // Check if more than 25% of the arrays have been removed
+    // Check if more than 25% of the arrays have been removed.
     if (filteredData.length < dataAsNumbers.length * 0.75) {
       throw new Error('Error processing file.')
     }
     return filteredData
   }
 
+  /**
+   * Normalize the given data using z-score normalization.
+   * This method scales the data to have a mean of 0 and a standard deviation of 1.
+   */
   private normalizeData (data: number[][]): number[][] {
+    // Calculate the mean for each column
     const means = data[0].map((_, colIndex) => mean(data.map(row => row[colIndex])))
+    // Calculate the standard deviation for each column
     const stdDevs = data[0].map((_, colIndex) => standardDeviation(data.map(row => row[colIndex])))
-
+    // Return the normalized data using z-score formula: (value - mean) / standardDeviation
     return data.map(row =>
       row.map((value, colIndex) =>
         (value - means[colIndex]) / stdDevs[colIndex]
@@ -70,23 +90,36 @@ export class KmeansLocalService {
     )
   }
 
+  /**
+   * Apply Principal Component Analysis (PCA) to reduce the dimensionality of the given data.
+   * This method projects the data into a 2D space while retaining as much variance as possible.
+   */
   private applyPCA (data: number[][]): number[][] {
     const pca = new PCA(data)
     return pca.predict(data, { nComponents: 2 }).to2DArray()
   }
 
+  /**
+   * One-hot encodes the categorical columns in the given data.
+   * Numeric columns are left unchanged, while categorical columns are transformed into binary columns
+   * where each unique category value becomes its own column.
+   */
   private oneHotEncode (data: string[][]): number[][] {
     const headers = data[0]
     const rows = data.slice(1)
+    // Helper function to check if a value is numeric.
     const isNumeric = (value: string): boolean => !isNaN(Number(value))
+    // Map to store unique values for each categorical column.
     const uniqueValuesPerColumn = new Map<number, string[]>()
 
+    // Identify categorical columns and store their unique values.
     headers.forEach((_, colIndex) => {
       const uniqueValues = [...new Set(rows.map(row => row[colIndex]))]
       if (!uniqueValues.every(isNumeric)) {
         uniqueValuesPerColumn.set(colIndex, uniqueValues)
       }
     })
+    // Convert each row to its one-hot encoded equivalent.
     return rows.map(row => {
       const encodedRow: number[] = []
       row.forEach((value, colIndex) => {
@@ -108,38 +141,56 @@ export class KmeansLocalService {
     })
   }
 
+  /**
+   * Performs KMeans clustering on the provided data file.
+   */
   async performKMeans (file: File, k: number, useOptK: boolean, distanceMetric: string, selectedIndices: number[]): Promise<ResponseInterface> {
+    // Convert the file to a 2D array
     this.data = await this.dataTo2DArrayService.dataTo2DArray(file)
+    // Filter the data based on selected indices
     this.data = this.data.map(row => { return selectedIndices.map(index => row[index]) })
+    // Remove rows with undefined or empty values
     this.data = this.data.filter(row => row.some(value => value !== undefined && value !== ''))
+    // One-hot encode categorical data
     let dataAsNumbers = this.oneHotEncode(this.data)
+    // Remove rows with NaN values
     dataAsNumbers = this.removeNaN(dataAsNumbers)
+    // Check if the data is multi-dimensional (more than 2 dimensions)
     let nDimensional = false
     if (dataAsNumbers[0].length > 2) {
       nDimensional = true
+      // Normalize the data and apply Principal Component Analysis (PCA) to reduce dimensions
       dataAsNumbers = this.normalizeData(dataAsNumbers)
       dataAsNumbers = this.applyPCA(dataAsNumbers)
     }
+    // If the useOptK flag is true, calculate the optimal number of clusters using the Elbow Method
     if (useOptK) {
       k = this.elbowMethod(dataAsNumbers, 100, distanceMetric)
     }
-
+    // Perform KMeans clustering
     const result = KMeans(dataAsNumbers, k, {
       distanceFunction: distanceMetric === 'EUCLIDEAN' ? this.euclideanDistance : this.manhattanDistance
     })
+    // Convert the clustering result to the desired JSON format
     return this.convertToJSONFormat(result, dataAsNumbers, file.name, distanceMetric, nDimensional)
   }
 
+  /**
+   * Converts the clustering result into a specified JSON format.
+   */
   private convertToJSONFormat (result: any, data: number[][], fileName: string, distanceMetric: string, nDimensional: boolean): ResponseInterface {
+    // Check for invalid data format
     if (this.data.length === 0 || this.data[0].length < 2) {
       console.error('Invalid CSV data format')
     }
+    // Determine the labels for the x and y axes
     let xLabel = this.data[0][0]
     let yLabel = this.data[0][1]
     if (nDimensional) {
       xLabel = 'PCA1'
       yLabel = 'PCA2'
     }
+    // Map the clustering result to the desired format
     const clusters = result.centroids.map((centroid: number[], index: number) => {
       const points = data.filter((_, dataIndex) => result.clusters[dataIndex] === index)
       return {
@@ -154,6 +205,7 @@ export class KmeansLocalService {
         }))
       }
     })
+    // Construct and return the final JSON response
     return {
       user_id: 0,
       request_id: 0,
